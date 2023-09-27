94d0fb9130e69534785a9a835a3a4e93897bbe05
jaydeepv14 <143499964+jaydeepv14@users.noreply.github.com>
2023-09-27 13:38:45 +0530

    image-viewer init

---
+++ projects/ngx-image-viewer/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-image-viewer
+ */
+
+export * from './lib/image-viewer-config.model';
+export * from './lib/image-viewer/image-viewer.component';
+export * from './lib/ngx-image-viewer.module';
